84833c2cd6dac4532dc79abb9c2160810daa2beb
laiba-navaid <laibanavaid234@gmail.com>
2026-04-30 16:50:02 +0500

    Initial Commit

--- Graph.cpp
+++ Graph.cpp
@@ -1,248 +1,440 @@
 #include <iostream>
 using namespace std;
 
-const int MAX = 10;
+#define SIZE 10
 
+char vertices[SIZE];
+int matrix[SIZE][SIZE] = {0};
+int vCount = 0;
 
-struct Graph{
-    int matrix[MAX][MAX];
-    int vertexCount;
-    bool isDirected;
-};
-
-
-struct Queue {
-    int data[MAX];
-    int front;
-    int rear;
-};
-
-void initQueue(Queue &q) {
-    q.front = -1;
-    q.rear = -1;
+// Get index of vertex
+int getIndex(char v) {
+    for (int i = 0; i < vCount; i++)
+        if (vertices[i] == v)
+            return i;
+    return -1;
 }
 
-void enqueue(Queue &q, int value) {
-    if (q.rear == MAX - 1) {
-        cout << "Queue FULL!\n";
+// Add vertex
+void addVertex(char v) {
+    if (getIndex(v) != -1) {
+        cout << "Vertex exists!\n";
         return;
     }
-    if (q.front == -1) q.front = 0;
-    q.data[++q.rear] = value;
+    vertices[vCount++] = v;
 }
 
-int dequeue(Queue &q) {
-    if (q.front == -1 || q.front > q.rear) {
-        return -1;
-    }
-    int val = q.data[q.front++];
-    if (q.front > q.rear) q.front = q.rear = -1;
-    return val;
-}
+// Add edge (undirected)
+void addEdge(char v1, char v2) {
+    int i = getIndex(v1);
+    int j = getIndex(v2);
 
-bool isEmpty(Queue &q) {
-    return q.front == -1;
-}
-
-
-struct Stack {
-    int data[MAX];
-    int top;
-};
-
-void initStack(Stack &s) {
-    s.top = -1;
-}
-
-void push(Stack &s, int value) {
-    if (s.top == MAX - 1) return;
-    s.data[++s.top] = value;
-}
-
-int pop(Stack &s) {
-    if (s.top == -1) return -1;
-    return s.data[s.top--];
-}
-
-bool isEmpty(Stack &s) {
-    return s.top == -1;
-}
-
-
-void initializeGraph(Graph &g){
-    g.vertexCount = 0;
-    for(int i=0;i<MAX;i++)
-        for(int j=0;j<MAX;j++)
-            g.matrix[i][j]=0;
-}
-
-void addVertex(Graph &g){
-    if(g.vertexCount >= MAX) return;
-    for(int i=0;i<g.vertexCount;i++){
-        g.matrix[g.vertexCount][i]=0;
-        g.matrix[i][g.vertexCount]=0;
-    }
-    g.vertexCount++;
-}
-
-void addEdge(Graph &g , int from , int to){
-    if(from<0||to<0||from>=g.vertexCount||to>=g.vertexCount) return;
-    g.matrix[from][to]=1;
-    if(!g.isDirected)
-        g.matrix[to][from]=1;
-}
-
-void removeVertex(Graph &g , int v){
-    if(v<0||v>=g.vertexCount) return;
-
-    for(int i=v;i<g.vertexCount-1;i++)
-        for(int j=0;j<g.vertexCount;j++)
-            g.matrix[i][j]=g.matrix[i+1][j];
-
-    for(int i=0;i<g.vertexCount-1;i++)
-        for(int j=v;j<g.vertexCount-1;j++)
-            g.matrix[i][j]=g.matrix[i][j+1];
-
-    g.vertexCount--;
-}
-
-void removeEdge(Graph &g , int from , int to){
-    if(from<0||to<0||from>=g.vertexCount||to>=g.vertexCount) return;
-    g.matrix[from][to]=0;
-    if(!g.isDirected)
-        g.matrix[to][from]=0;
-}
-
-void displayGraph(Graph &g){
-    cout<<"\nMatrix:\n  ";
-    for(int i=0;i<g.vertexCount;i++) cout<<i<<" ";
-    cout<<endl;
-
-    for(int i=0;i<g.vertexCount;i++){
-        for(int j=0;j<g.vertexCount;j++)
-            cout<<g.matrix[i][j]<<" ";
-        cout<<endl;
-    }
-}
-
-
-void BFS(Graph &g , int start){
-    bool visited[MAX]={false};
-    Queue q;
-    initQueue(q);
-
-    visited[start]=true;
-    enqueue(q,start);
-
-    cout<<"BFS: ";
-    while(!isEmpty(q)){
-        int cur=dequeue(q);
-        cout<<cur<<" ";
-
-        for(int j=0;j<g.vertexCount;j++){
-            if(g.matrix[cur][j]==1 && !visited[j]){
-                visited[j]=true;
-                enqueue(q,j);
-            }
-        }
-    }
-    cout<<endl;
-}
-
-
-void DFS(Graph &g , int start){
-    bool visited[MAX]={false};
-    Stack s;
-    initStack(s);
-
-    push(s,start);
-    visited[start]=true;
-
-    cout<<"DFS: ";
-    while(!isEmpty(s)){
-        int cur=pop(s);
-        cout<<cur<<" ";
-
-        for(int j=0;j<g.vertexCount;j++){
-            if(g.matrix[cur][j]==1 && !visited[j]){
-                visited[j]=true;
-                push(s,j);
-            }
-        }
-    }
-    cout<<endl;
-}
-
-// BFS based 
-void findPath(Graph &g, int start, int end){
-    bool visited[MAX]={false};
-    int parent[MAX];
-
-    for(int i=0;i<MAX;i++) parent[i]=-1;
-
-    Queue q;
-    initQueue(q);
-
-    visited[start]=true;
-    enqueue(q,start);
-
-    while(!isEmpty(q)){
-        int cur=dequeue(q);
-
-        for(int j=0;j<g.vertexCount;j++){
-            if(g.matrix[cur][j]==1 && !visited[j]){
-                visited[j]=true;
-                parent[j]=cur;
-                enqueue(q,j);
-            }
-        }
-    }
-
-    if(!visited[end]){
-        cout<<"No Path!\n";
+    if (i == -1 || j == -1) {
+        cout << "Invalid vertices\n";
         return;
     }
 
-    Stack s;
-    initStack(s);
+    matrix[i][j] = 1;
+    matrix[j][i] = 1;
+}
 
-    int v=end;
-    while(v!=-1){
-        push(s,v);
-        v=parent[v];
-    }
+// Remove edge
+void removeEdge(char v1, char v2) {
+    int i = getIndex(v1);
+    int j = getIndex(v2);
 
-    cout<<"Path: ";
-    while(!isEmpty(s)){
-        cout<<pop(s);
-        if(!isEmpty(s)) cout<<" -> ";
+    if (i == -1 || j == -1) return;
+
+    matrix[i][j] = 0;
+    matrix[j][i] = 0;
+
+    cout << "Edge removed\n";
+}
+
+// Remove vertex
+void removeVertex(char v) {
+    int idx = getIndex(v);
+    if (idx == -1) return;
+
+    // Shift vertices
+    for (int i = idx; i < vCount - 1; i++)
+        vertices[i] = vertices[i + 1];
+
+    // Shift rows up
+    for (int i = idx; i < vCount - 1; i++)
+        for (int j = 0; j < vCount; j++)
+            matrix[i][j] = matrix[i + 1][j];
+
+    // Shift cols left
+    for (int j = idx; j < vCount - 1; j++)
+        for (int i = 0; i < vCount; i++)
+            matrix[i][j] = matrix[i][j + 1];
+
+    vCount--;
+
+    cout << "Vertex removed\n";
+}
+
+void display() {
+    cout << "\nMatrix:\n   ";
+    for (int i = 0; i < vCount; i++)
+        cout << vertices[i] << " ";
+    cout << endl;
+
+    for (int i = 0; i < vCount; i++) {
+        cout << vertices[i] << "  ";
+        for (int j = 0; j < vCount; j++)
+            cout << matrix[i][j] << " ";
+        cout << endl;
     }
-    cout<<endl;
 }
 
 
-int main(){
-    Graph g;
-    int ch,from,to,v,start,end;
+void findDegree(char v) {
+    int i = getIndex(v);
+    if (i == -1) return;
 
-    initializeGraph(g);
+    int degree = 0;
+    for (int j = 0; j < vCount; j++)
+        if (matrix[i][j] == 1)
+            degree++;
 
-    cout<<"Directed? (1/0): ";
-    cin>>g.isDirected;
+    cout << "Degree of " << v << " = " << degree << endl;
+}
 
-    while(true){
-        cout<<"\n1.Add Vertex\n2.Add Edge\n3.Remove Edge\n4.Remove Vertex";
-        cout<<"\n5.Display\n6.BFS\n7.DFS\n8.Find Path\n9.Exit\nChoice: ";
-        cin>>ch;
 
-        if(ch==1) addVertex(g);
-        else if(ch==2){ cin>>from>>to; addEdge(g,from,to); }
-        else if(ch==3){ cin>>from>>to; removeEdge(g,from,to); }
-        else if(ch==4){ cin>>v; removeVertex(g,v); }
-        else if(ch==5) displayGraph(g);
-        else if(ch==6){ cin>>start; BFS(g,start); }
-        else if(ch==7){ cin>>start; DFS(g,start); }
-        else if(ch==8){ cin>>start>>end; findPath(g,start,end); }
-        else if(ch==9) break;
+
+bool isAdjacent(char v1, char v2) {
+    int i = getIndex(v1);
+    int j = getIndex(v2);
+
+    if (i == -1 || j == -1) return false;
+
+    if (matrix[i][j] == 1) {
+        cout << "Adjacent\n";
+        return true;
     }
+    cout << "Not Adjacent\n";
+    return false;
+}
+
+
+void findNeighbours(char v) {
+    int i = getIndex(v);
+    if (i == -1) return;
+
+    cout << "Neighbours of " << v << ": ";
+
+    for (int j = 0; j < vCount; j++) {
+        if (matrix[i][j] == 1)
+            cout << vertices[j] << " ";
+    }
+    cout << endl;
+}
+
+
+
+void BFS(char start) {
+    bool visited[SIZE] = {false};
+    int queue[SIZE], front = 0, rear = 0;
+
+    int s = getIndex(start);
+    if (s == -1) return;
+
+    queue[rear++] = s;
+    visited[s] = true;
+
+    cout << "BFS: ";
+
+    while (front < rear) {
+        int curr = queue[front++];
+        cout << vertices[curr] << " ";
+
+        for (int i = 0; i < vCount; i++) {
+            if (matrix[curr][i] == 1 && !visited[i]) {
+                visited[i] = true;
+                queue[rear++] = i;
+            }
+        }
+    }
+    cout << endl;
+}
+
+
+void DFS(char start) {
+    bool visited[SIZE] = {false};
+    int stack[SIZE], top = -1;
+
+    int s = getIndex(start);
+    if (s == -1) return;
+
+    stack[++top] = s;
+
+    cout << "DFS: ";
+
+    while (top != -1) {
+        int curr = stack[top--];
+
+        if (!visited[curr]) {
+            visited[curr] = true;
+            cout << vertices[curr] << " ";
+
+            for (int i = vCount - 1; i >= 0; i--) {
+                if (matrix[curr][i] == 1 && !visited[i])
+                    stack[++top] = i;
+            }
+        }
+    }
+    cout << endl;
+}
+
+
+
+void findPath(char src, char dest) {
+    bool visited[SIZE] = {false};
+    int stack[SIZE], top = -1;
+
+    int path[SIZE], len = 0;
+
+    int s = getIndex(src);
+    int d = getIndex(dest);
+
+    stack[++top] = s;
+
+    while (top != -1) {
+        int curr = stack[top--];
+
+        if (!visited[curr]) {
+            visited[curr] = true;
+            path[len++] = curr;
+
+            if (curr == d) {
+                cout << "Path: ";
+                for (int i = 0; i < len; i++)
+                    cout << vertices[path[i]] << " ";
+                cout << endl;
+                return;
+            }
+
+            for (int i = 0; i < vCount; i++) {
+                if (matrix[curr][i] == 1 && !visited[i])
+                    stack[++top] = i;
+            }
+        }
+    }
+
+    cout << "No Path\n";
+}
+
+
+bool isConnected(char v1, char v2) {
+    bool visited[SIZE] = {false};
+    int stack[SIZE], top = -1;
+
+    int s = getIndex(v1);
+    int d = getIndex(v2);
+
+    stack[++top] = s;
+
+    while (top != -1) {
+        int curr = stack[top--];
+
+        if (!visited[curr]) {
+            visited[curr] = true;
+
+            if (curr == d) {
+                cout << "Connected\n";
+                return true;
+            }
+
+            for (int i = 0; i < vCount; i++)
+                if (matrix[curr][i] == 1 && !visited[i])
+                    stack[++top] = i;
+        }
+    }
+
+    cout << "Not Connected\n";
+    return false;
+}
+
+
+bool isGraphConnected() {
+    bool visited[SIZE] = {false};
+    int queue[SIZE], front = 0, rear = 0;
+
+    queue[rear++] = 0;
+    visited[0] = true;
+
+    while (front < rear) {
+        int curr = queue[front++];
+
+        for (int i = 0; i < vCount; i++) {
+            if (matrix[curr][i] == 1 && !visited[i]) {
+                visited[i] = true;
+                queue[rear++] = i;
+            }
+        }
+    }
+
+    for (int i = 0; i < vCount; i++) {
+        if (!visited[i]) {
+            cout << "Graph NOT connected\n";
+            return false;
+        }
+    }
+
+    cout << "Graph connected\n";
+    return true;
+}
+
+
+bool isCyclic() {
+    bool visited[SIZE] = {false};
+    int parent[SIZE] = {-1};
+
+    int stack[SIZE], top = -1;
+
+    stack[++top] = 0;
+
+    while (top != -1) {
+        int curr = stack[top--];
+
+        if (!visited[curr]) {
+            visited[curr] = true;
+
+            for (int i = 0; i < vCount; i++) {
+                if (matrix[curr][i] == 1) {
+                    if (!visited[i]) {
+                        parent[i] = curr;
+                        stack[++top] = i;
+                    }
+                    else if (parent[curr] != i) {
+                        cout << "Cycle Found\n";
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+
+    cout << "No Cycle\n";
+    return false;
+}
+
+int main() {
+    int choice;
+    char v1, v2;
+
+    do {
+        cout << "\n========= MATRIX GRAPH MENU =========";
+        cout << "\n1. Add Vertex";
+        cout << "\n2. Add Edge";
+        cout << "\n3. Remove Vertex";
+        cout << "\n4. Remove Edge";
+        cout << "\n5. Display Graph";
+        cout << "\n6. Find Degree";
+        cout << "\n7. Check Adjacent";
+        cout << "\n8. Find Neighbours";
+        cout << "\n9. BFS";
+        cout << "\n10. DFS";
+        cout << "\n11. Find Path";
+        cout << "\n12. Check Connected (Two Vertices)";
+        cout << "\n13. Check Graph Connected";
+        cout << "\n14. Check Cycle";
+        cout << "\n15. Exit";
+
+        cout << "\nEnter choice: ";
+        cin >> choice;
+
+        switch (choice) {
+
+        case 1:
+            cout << "Enter vertex: ";
+            cin >> v1;
+            addVertex(v1);
+            break;
+
+        case 2:
+            cout << "Enter two vertices: ";
+            cin >> v1 >> v2;
+            addEdge(v1, v2);
+            break;
+
+        case 3:
+            cout << "Enter vertex to remove: ";
+            cin >> v1;
+            removeVertex(v1);
+            break;
+
+        case 4:
+            cout << "Enter edge (v1 v2): ";
+            cin >> v1 >> v2;
+            removeEdge(v1, v2);
+            break;
+
+        case 5:
+            display();
+            break;
+
+        case 6:
+            cout << "Enter vertex: ";
+            cin >> v1;
+            findDegree(v1);
+            break;
+
+        case 7:
+            cout << "Enter two vertices: ";
+            cin >> v1 >> v2;
+            isAdjacent(v1, v2);
+            break;
+
+        case 8:
+            cout << "Enter vertex: ";
+            cin >> v1;
+            findNeighbours(v1);
+            break;
+
+        case 9:
+            cout << "Enter start vertex: ";
+            cin >> v1;
+            BFS(v1);
+            break;
+
+        case 10:
+            cout << "Enter start vertex: ";
+            cin >> v1;
+            DFS(v1);
+            break;
+
+        case 11:
+            cout << "Enter source and destination: ";
+            cin >> v1 >> v2;
+            findPath(v1, v2);
+            break;
+
+        case 12:
+            cout << "Enter two vertices: ";
+            cin >> v1 >> v2;
+            isConnected(v1, v2);
+            break;
+
+        case 13:
+            isGraphConnected();
+            break;
+
+        case 14:
+            isCyclic();
+            break;
+
+        case 15:
+            cout << "Exiting...\n";
+            break;
+
+        default:
+            cout << "Invalid choice!\n";
+        }
+
+    } while (choice != 15);
+
+    return 0;
 }
